84df17b715cb5a71153ed2f27df84d33d7b63a4a
Rajiv Kulkarni <63727801+rajivk16@users.noreply.github.com>
2024-05-16 21:37:38 +0000

    added context and dynamic rendering

--- context/WalletContext.js
+++ context/WalletContext.js
@@ -1,33 +1,81 @@
-import { createContext, useContext, useMemo } from 'react';
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from '@solana/wallet-adapter-react';
-import {
-  WalletModalProvider,
-  WalletDisconnectButton,
-  WalletMultiButton,
-} from '@solana/wallet-adapter-react-ui';
-import {
-  PhantomWalletAdapter,
-  SolletWalletAdapter,
-} from '@solana/wallet-adapter-wallets';
+// context/WalletContext.js
+import React, { createContext, useContext, useEffect, useMemo, useState } from 'react';
 
 const WalletContext = createContext();
 
 export const WalletContextProvider = ({ children }) => {
-  const wallets = useMemo(
-    () => [new PhantomWalletAdapter(), new SolletWalletAdapter()],
-    []
+  const [wallet, setWallet] = useState(null);
+  const [publicKey, setPublicKey] = useState(null);
+  const [connected, setConnected] = useState(false);
+
+  useEffect(() => {
+    const initializeWallet = async () => {
+      const Wallet = (await import('@project-serum/sol-wallet-adapter')).default;
+      const providerUrl = 'https://www.sollet.io';
+      const walletInstance = new Wallet(providerUrl);
+
+      walletInstance.on('connect', (publicKey) => {
+        console.log('Connected to ' + publicKey.toBase58());
+        setPublicKey(publicKey);
+        setConnected(true);
+      });
+
+      walletInstance.on('disconnect', () => {
+        console.log('Disconnected');
+        setPublicKey(null);
+        setConnected(false);
+      });
+
+      setWallet(walletInstance);
+    };
+
+    initializeWallet();
+  }, []);
+
+  const connectWallet = async () => {
+    if (wallet) {
+      await wallet.connect();
+    }
+  };
+
+  const disconnectWallet = async () => {
+    if (wallet) {
+      await wallet.disconnect();
+    }
+  };
+
+  const sendTransaction = async (transaction) => {
+    if (wallet && publicKey) {
+      const { Connection } = await import('@solana/web3.js');
+      const connection = new Connection('https://api.mainnet-beta.solana.com');
+      const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash();
+      transaction.recentBlockhash = blockhash;
+      transaction.feePayer = publicKey;
+      const signed = await wallet.signTransaction(transaction);
+      const txid = await connection.sendRawTransaction(signed.serialize());
+      await connection.confirmTransaction({
+        signature: txid,
+        blockhash,
+        lastValidBlockHeight,
+      });
+      return txid;
+    }
+    return null;
+  };
+
+  const value = useMemo(
+    () => ({
+      wallet,
+      publicKey,
+      connected,
+      connectWallet,
+      disconnectWallet,
+      sendTransaction,
+    }),
+    [wallet, publicKey, connected]
   );
 
-  return (
-    <ConnectionProvider endpoint="https://api.mainnet-beta.solana.com">
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>{children}</WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  );
+  return <WalletContext.Provider value={value}>{children}</WalletContext.Provider>;
 };
 
 export const useWallet = () => useContext(WalletContext);
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import '../globals.css';
-import { WalletContextProvider } from '../context/WalletContext';
+// pages/_app.js
+import '../global.css';
+import dynamic from 'next/dynamic';
+import App from 'next/app'; // Import App from next/app
+
+
+const WalletContextProvider = dynamic(() => import('../context/WalletContext').then(mod => mod.WalletContextProvider), {
+  ssr: false,
+});
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -8,5 +15,10 @@ function MyApp({ Component, pageProps }) {
     </WalletContextProvider>
   );
 }
+
+MyApp.getInitialProps = async (appContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  return { ...appProps };
+};
 
 export default MyApp;
